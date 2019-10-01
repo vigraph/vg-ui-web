c18741565ddc8dd30492d3049480fb9762186de1
Tom Nicholls <tom@packetship.com>
2019-10-01 10:06:30 +0100

    ColourPicker now handles #rgb as well as #rrggbb

--- src/ColourPicker.tsx
+++ src/ColourPicker.tsx
@@ -564,6 +564,14 @@ export default class ColourPicker extends React.Component<IProps, IState>
     private hexToRGB = (hex: string) =>
     {
       hex = hex.replace('#','');
+
+      // Convert [#]rgb to [#]rrggbb
+      if (hex.length === 3)
+      {
+        hex = hex.substring(0,1) + hex.substring(0,1) + hex.substring(1,2) +
+          hex.substring(1,2) + hex.substring(2,3) + hex.substring(2,3);
+      }
+
       const r = parseInt(hex.substring(0,2), 16) / 255;
       const g = parseInt(hex.substring(2,4), 16) / 255;
       const b = parseInt(hex.substring(4,6), 16) / 255;
