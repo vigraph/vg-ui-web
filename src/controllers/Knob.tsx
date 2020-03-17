6f6684e89fe49cf3fe968086b2947441be9a8eba
Tom Nicholls <tom@packetship.com>
2020-03-17 14:06:55 +0000

    Improve Knob control
    Clicking on knob arc (background) updates value to clicked position
    Increased sensitivity of slider knob control method
    
    BUG:COMMENT:1590

--- src/controllers/Knob.tsx
+++ src/controllers/Knob.tsx
@@ -33,9 +33,11 @@ export default class Knob extends React.Component<IProps, IState>
   }
 
   private property: Model.Property;
+  private knobRef: SVGSVGElement | null;
 
   private arcStart: {x: number, y: number};
-  private pointerStart: {x: number, y: number};
+  private pointerPosition: {x: number, y: number};
+  private pointerDown: {x: number, y: number};
   private circleCentre: {x: number, y: number};
   private range: number;
 
@@ -46,6 +48,7 @@ export default class Knob extends React.Component<IProps, IState>
     super(props);
 
     this.property = props.property;
+    this.knobRef = null;
 
     const knobSettings = vgConfig.Controls.knob;
 
@@ -59,7 +62,8 @@ export default class Knob extends React.Component<IProps, IState>
     };
 
     this.arcStart = {x: this.settings.radius, y: 2*this.settings.radius};
-    this.pointerStart = {x: 0, y: 0};
+    this.pointerDown = {x: 0, y: 0};
+    this.pointerPosition = {x: 0, y: 0};
     this.circleCentre = {x: 0, y: 0};
 
     this.range = this.settings.rangeMax - this.settings.rangeMin;
@@ -78,28 +82,8 @@ export default class Knob extends React.Component<IProps, IState>
       vgUtils.log("Knob Error - Logarithmic control minimum range");
     }
 
-    const propRange = Object.assign({}, this.property.range);
-    propRange.min = (propRange.min !== undefined ? propRange.min : 0);
-    propRange.max = (propRange.max !== undefined ? propRange.max : 1);
-
-    const rangeMin = settings.logControl ? Math.log10(propRange.min) :
-      propRange.min;
-    const rangeMax = settings.logControl ? Math.log10(propRange.max) :
-      propRange.max;
-    let currentValue = settings.logControl ?
-      Math.log10(this.state.currentValue) : this.state.currentValue;
-
-    currentValue = Math.min(currentValue, rangeMax);
-    currentValue = Math.max(currentValue, rangeMin);
-
-    if (isNaN(currentValue))
-    {
-      currentValue = rangeMin;
-    }
-
     // Current position in degrees from 0
-    const currentPos = ((currentValue - rangeMin) /
-      (rangeMax - rangeMin) * this.range) + settings.rangeMin;
+    const currentPos = this.valueToPosition(this.state.currentValue);
 
     const arcEnd = this.positionToCords(currentPos);
     const arcSweep = currentPos > 180 ? 1 : 0;
@@ -110,6 +94,7 @@ export default class Knob extends React.Component<IProps, IState>
     return(
         <svg id="knob" className={this.props.settingsType + " " +
           this.props.property.valueType}
+          ref={(ref) => {this.knobRef = ref}}
           height={r*2} width={r*2}
           onPointerDown={this.handlePointerDown}>
           <path
@@ -120,6 +105,7 @@ export default class Knob extends React.Component<IProps, IState>
               `${backgroundEnd.x},${backgroundEnd.y} z`}
             transform={`scale(${this.state.turning ? settings.turnScale :
               1}) rotate(${settings.offset}, ${r}, ${r})`}
+            onPointerUp={this.handleBackgroundPointerUp}
           />
           <path
             className={`knob-arc`}
@@ -144,6 +130,58 @@ export default class Knob extends React.Component<IProps, IState>
     );
   }
 
+  // Click on knob background arc updates value to clicked position
+  private handleBackgroundPointerUp = (e: React.PointerEvent<SVGPathElement>) =>
+  {
+    const pointerUp = {x:0, y:0};
+    pointerUp.x = e.pageX;
+    pointerUp.y = e.pageY;
+    const svgPointerUp = vgUtils.windowToSVGPosition(pointerUp, this.knobRef);
+
+    if (pointerUp.x === this.pointerDown.x && pointerUp.y ===
+      this.pointerDown.y)
+    {
+      // Centre on circle centre
+      const radius = this.settings.radius;
+      const pUpX = svgPointerUp.x - radius;
+      const pUpY = svgPointerUp.y - radius;
+      const arcStartX = this.arcStart.x - radius;
+      const arcStartY = this.arcStart.y - radius;
+
+      // Calculate angle between arc start position and current pointer posiiton
+      const dot = (pUpX * arcStartX) + (pUpY * arcStartY);
+      const det = (pUpX * arcStartY) - (pUpY * arcStartX);
+      const angleRad = Math.atan2(det, dot);
+      const angle = angleRad * (180 / Math.PI);
+
+      // Current position in degrees from 0
+      let newPos = angle;
+
+      if (newPos < 0)
+      {
+        newPos = newPos * -1;
+      }
+      else
+      {
+        newPos = 360 - newPos;
+      }
+
+      newPos -= this.settings.offset;
+
+      const newValue = this.positionToValue(newPos);
+
+      if (this.state.currentValue !== newValue)
+      {
+        this.setState({currentValue: newValue});
+
+        if (this.props.update)
+        {
+          this.props.update(newValue);
+        }
+      }
+    }
+  }
+
   private handlePointerDown = (e: React.PointerEvent<SVGElement>) =>
   {
     e.stopPropagation();
@@ -155,9 +193,12 @@ export default class Knob extends React.Component<IProps, IState>
     this.circleCentre.y = e.currentTarget.getBoundingClientRect().top +
       this.settings.radius;
 
+    this.pointerDown.x = e.pageX;
+    this.pointerDown.y = e.pageY;
+
     // Pointer start coordinates with circle centre as origin
-    this.pointerStart.x = e.pageX - this.circleCentre.x;
-    this.pointerStart.y = e.pageY - this.circleCentre.y;
+    this.pointerPosition.x = e.pageX - this.circleCentre.x;
+    this.pointerPosition.y = e.pageY - this.circleCentre.y;
 
     this.setState({turning: true});
 
@@ -172,6 +213,9 @@ export default class Knob extends React.Component<IProps, IState>
     window.removeEventListener('pointerup', this.handlePointerUp);
     window.removeEventListener('pointermove', this.handlePointerMove);
 
+    this.pointerDown.x = 0;
+    this.pointerDown.y = 0;
+
     this.setState({turning: false});
 
     if (this.props.endUpdate)
@@ -182,34 +226,26 @@ export default class Knob extends React.Component<IProps, IState>
 
   private handlePointerMove = (e: PointerEvent) =>
   {
+    this.updateValue(this.settings.controlMode, e.pageX, e.pageY);
+  }
+
+  private updateValue = (controlMode: string, pointerX: number,
+    pointerY: number) =>
+  {
     // Current pointer position coordinates with circle centre as origin
-    const currentX = e.pageX - this.circleCentre.x;
-    const currentY = e.pageY - this.circleCentre.y;
+    const currentX = pointerX - this.circleCentre.x;
+    const currentY = pointerY - this.circleCentre.y;
 
-    const settings = this.settings;
-
-    const propRange = Object.assign({}, this.property.range);
-    propRange.min = (propRange.min !== undefined ? propRange.min : 0);
-    propRange.max = (propRange.max !== undefined ? propRange.max : 1);
-
-    const rangeMin = settings.logControl ? Math.log10(propRange.min) :
-      propRange.min;
-    const rangeMax = settings.logControl ? Math.log10(propRange.max) :
-      propRange.max;
-    const currentValue = settings.logControl ?
-      Math.log10(this.state.currentValue) : this.state.currentValue;
-
-    const currentPos = ((currentValue - rangeMin) /
-      (rangeMax - rangeMin) * this.range) + this.settings.rangeMin
+    const currentPos = this.valueToPosition(this.state.currentValue);
 
     let newPos;
 
     // Knob control mode
-    if (this.settings.controlMode === "physical")
+    if (controlMode === "physical")
     {
       newPos = this.physicalMode(currentX, currentY, currentPos);
     }
-    else if (this.settings.controlMode === "slider")
+    else if (controlMode === "slider")
     {
       newPos = this.sliderMode(currentX, currentY, currentPos);
     }
@@ -220,17 +256,11 @@ export default class Knob extends React.Component<IProps, IState>
 
     newPos = this.limitPosition(newPos);
 
-    let newValue = ((newPos / settings.rangeMax) * (rangeMax -
-      rangeMin)) + rangeMin;
-
-    if (settings.logControl)
-    {
-      newValue = Math.pow(10, newValue);
-    }
+    const newValue = this.positionToValue(newPos);
 
     // Pointer start can now be current pointer coords
-    this.pointerStart.x = currentX;
-    this.pointerStart.y = currentY;
+    this.pointerPosition.x = currentX;
+    this.pointerPosition.y = currentY;
 
     if (this.state.currentValue !== newValue)
     {
@@ -247,8 +277,8 @@ export default class Knob extends React.Component<IProps, IState>
   private basicMode = (currentX: number, currentY: number,
     currentPos: number) =>
   {
-    const distance = Math.hypot(currentX - this.pointerStart.x, currentY -
-      this.pointerStart.y) * Math.sign(currentX - this.pointerStart.x);
+    const distance = Math.hypot(currentX - this.pointerPosition.x, currentY -
+      this.pointerPosition.y) * Math.sign(currentX - this.pointerPosition.x);
 
     const newPos = currentPos + distance;
 
@@ -261,8 +291,8 @@ export default class Knob extends React.Component<IProps, IState>
   {
     // Calculate angle between pointer start position and current pointer posiiton
     // and add to current knob position (angle)
-    const dot = (this.pointerStart.x * currentX) + (this.pointerStart.y * currentY);
-    const det = (this.pointerStart.x * currentY) - (this.pointerStart.y * currentX);
+    const dot = (this.pointerPosition.x * currentX) + (this.pointerPosition.y * currentY);
+    const det = (this.pointerPosition.x * currentY) - (this.pointerPosition.y * currentX);
     const angleRad = Math.atan2(det, dot);
     const angle = angleRad * (180 / Math.PI);
 
@@ -278,9 +308,9 @@ export default class Knob extends React.Component<IProps, IState>
     currentPos: number) =>
   {
     const diffX = currentX;
-    const diffY = (currentY - this.pointerStart.y) * (-1);
+    const diffY = (currentY - this.pointerPosition.y) * (-1);
 
-    const diff = (diffY * (Math.abs(diffX) / 500));
+    const diff = (diffY * (Math.abs(diffX) / 200));
 
     const newPos = currentPos + diff;
 
@@ -346,5 +376,59 @@ export default class Knob extends React.Component<IProps, IState>
     }
 
     return {x: newX, y: newY}
+  }
+
+  // Convert value to position (degrees from 0)
+  private valueToPosition = (value: number) =>
+  {
+    const settings = this.settings;
+    const propRange = Object.assign({}, this.property.range);
+    propRange.min = (propRange.min !== undefined ? propRange.min : 0);
+    propRange.max = (propRange.max !== undefined ? propRange.max : 1);
+
+    const rangeMin = settings.logControl ? Math.log10(propRange.min) :
+      propRange.min;
+    const rangeMax = settings.logControl ? Math.log10(propRange.max) :
+      propRange.max;
+    let tempValue = settings.logControl ? Math.log10(value) : value;
+
+    tempValue = Math.min(tempValue, rangeMax);
+    tempValue = Math.max(tempValue, rangeMin);
+
+    if (isNaN(tempValue))
+    {
+      tempValue = rangeMin;
+    }
+
+    // Current position in degrees from 0
+    const currentPos = ((tempValue - rangeMin) /
+      (rangeMax - rangeMin) * this.range) + settings.rangeMin;
+
+    return currentPos;
+  }
+
+  // Conver position (degrees from 0) to output value
+  private positionToValue = (position: number) =>
+  {
+    const settings = this.settings;
+
+    const propRange = Object.assign({}, this.property.range);
+    propRange.min = (propRange.min !== undefined ? propRange.min : 0);
+    propRange.max = (propRange.max !== undefined ? propRange.max : 1);
+
+    const rangeMin = settings.logControl ? Math.log10(propRange.min) :
+      propRange.min;
+    const rangeMax = settings.logControl ? Math.log10(propRange.max) :
+      propRange.max;
+
+    let newValue = ((position / settings.rangeMax) * (rangeMax - rangeMin)) +
+      rangeMin;
+
+    if (this.settings.logControl)
+    {
+      newValue = Math.pow(10, newValue);
+    }
+
+    return newValue;
   }
 }
