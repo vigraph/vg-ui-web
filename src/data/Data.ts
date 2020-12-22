1cd27bd6b35701118edca026d870af75a8bb1da5
Paul Clark <paul@sandtreader.com>
2020-12-22 15:38:45 +0000

    Remove redundant getConnectorPosition()

--- src/data/Data.ts
+++ src/data/Data.ts
@@ -1093,12 +1093,6 @@ class Data
       }
     }
 
-    // Get connector position from properties config for a given input/output id
-    const getConnectorPosition = (id: string) =>
-    {
-      return {};
-    }
-
     // Inputs - position added later
     const gInputs:
       Array<{ id: string, type: string, sampleRate: number, x?: number,
@@ -1113,7 +1107,7 @@ class Data
           item.inputs[inputID].sample_rate:0
 
         gInputs.push({id: inputID, type: mInputs[inputID].type, sampleRate:
-          sampleRate ? sampleRate : 0, ...getConnectorPosition(inputID)});
+          sampleRate ? sampleRate : 0});
       }
     }
 
@@ -1126,8 +1120,7 @@ class Data
       for (const outputID of Object.keys(mOutputs))
       {
         gOutputs.push({id: outputID, type: mOutputs[outputID].type,
-          sampleRate: item.outputs?item.outputs[outputID].sample_rate:0,
-          ...getConnectorPosition(outputID)});
+          sampleRate: item.outputs?item.outputs[outputID].sample_rate:0});
       }
     }
 
@@ -1139,8 +1132,7 @@ class Data
         for (const iID of Object.keys(item.inputs))
         {
           gInputs.push({id: iID, type: item.inputs[iID].type,
-            sampleRate: item.inputs[iID].sample_rate || 0,
-            ...getConnectorPosition(iID)});
+            sampleRate: item.inputs[iID].sample_rate || 0});
         }
       }
 
@@ -1149,8 +1141,7 @@ class Data
         for (const oID of Object.keys(item.outputs))
         {
           gOutputs.push({id: oID, type: item.outputs[oID].type,
-            sampleRate: item.outputs[oID].sample_rate,
-            ...getConnectorPosition(oID)});
+            sampleRate: item.outputs[oID].sample_rate});
         }
       }
     }
